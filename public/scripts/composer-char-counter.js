fd34701b5e5c252f996a0034209fe4b69db75afd
Aaron <aaron3993@hotmail.com>
2020-06-19 02:42:49 +0000

    Added two screenshots

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -11,38 +11,6 @@ const characterCount = function() {
   });
 };
 
-const toggleForm = () => {
-  $(".toggle-button").click(function() {
-    $(".form-hidden").slideToggle();
-  });
-};
-
-const scroll = function() {
-  $(window).on('scroll', function() {
-    scrollPosition = $(this).scrollTop();
-    if (scrollPosition > 0) {
-      $('#scroll-up').fadeIn();
-      $('.toggle-button').animate({opacity: 0}, 0);
-    } else {
-      $('#scroll-up').fadeOut();
-      $('.toggle-button').animate({opacity: 1});
-    }
-  });
-};
-
-const scrollToForm = function() {
-  $("#scroll-up").click(function() {
-    $('html, body').animate({
-      scrollTop: $("body")
-    }, 0);
-    $(".form-hidden").slideDown();
-    $("#tweet-text").focus();
-  });
-};
-
 $(document).ready(function() {
-  toggleForm();
   characterCount();
-  scroll();
-  scrollToForm();
 });
